cde2233a6010e4bbc73dd946172b19a1571c051e
Bruna Hosken <Bruna@TP-LPT-0081.local>
2026-08-31 19:12:21 -0300

    config dev environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { createObserveModule } from '@nestjs/observe';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
+import { Module } from '@nestjs/common'
+import { createObserveModule } from '@nestjs/observe'
+import { AppController } from './app.controller.js'
+import { AppService } from './app.service.js'
 
-export const { ObserveModule, ObserveInstrument } = createObserveModule();
+export const { ObserveModule, ObserveInstrument } = createObserveModule()
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core'
 import {
   FastifyAdapter,
-  NestFastifyApplication
-} from '@nestjs/platform-fastify';
-import { AppModule, ObserveInstrument } from './app.module.js';
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
+import { AppModule, ObserveInstrument } from './app.module.js'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, 
-    new FastifyAdapter()
-  );
-  await app.listen(3000, '0.0.0.0');
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  )
+  await app.listen(3000, '0.0.0.0')
 }
-await bootstrap();
+await bootstrap()
