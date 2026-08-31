2cf6ba16f238e4efc65d7dcff23a59f8b1d6c0cf
Bruna Hosken <Bruna@TP-LPT-0081.local>
2026-08-31 18:37:55 -0300

    First commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify';
+import { AppModule, ObserveInstrument } from './app.module.js';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, 
+    new FastifyAdapter()
+  );
+  await app.listen(3000, '0.0.0.0');
+}
+await bootstrap();
